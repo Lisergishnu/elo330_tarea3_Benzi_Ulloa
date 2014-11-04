1c62a14f05ad4e4926d5d23267ab12809458d9da
Lisergishnu <marco.benzi@alumnos.usm.cl>
2014-11-03 23:15:12 -0300

    Better lock

--- src/erp_udp.c
+++ src/erp_udp.c
@@ -173,144 +173,149 @@ void* senderThread(void* arg)
 	servaddr.sin_addr.s_addr = inet_addr(info->remote_host);
 	servaddr.sin_port = htons(info->remote_port);
 
-	for(;;)
-		{
-			if(NULL != info->mesgList[sendIndex])
-				{
-					int error;
-					if ((error = pthread_mutex_lock( &listMutex )) != 0)
-						fprintf(stderr,"ERROR: %d\n", error);
-					else
-						{
-							strcpy(mesg, info->mesgList[sendIndex]->msg);
-							lagger(delay_avg, delay_variation);
+  /* Temporal datagram */
+  datagram temp;
 
-							if(strcmp(mesg, "") != 0)
-								sendto(sockfd, mesg, strlen(mesg), 0,(struct sockaddr*) &servaddr, sizeof(servaddr));
+  for(;;)
+  {
+    int error;
+    if ((error = pthread_mutex_lock( &listMutex )) != 0)
+      fprintf(stderr,"ERROR: %d\n", error);
+    else
+    {
+      if(NULL != info->mesgList[sendIndex])
+      {
+        strcpy(temp.msg,info->mesgList[sendIndex]->msg);
+        temp.timestamp = info->mesgList[sendIndex]->timestamp;
+        free(info->mesgList[sendIndex]);
+        info->mesgList[sendIndex] = NULL;
+        sendIndex = (sendIndex+1) % LIST_SIZE;
+        pthread_mutex_unlock( &listMutex );
 
-							struct timespec outTime;
-							clock_gettime(CLOCK_MONOTONIC,&outTime);
 
-							printf("Mesg: %s\nmesgList[%d]: %s\nOutbound timestamp: %lld.%09ld [s]\n", mesg, sendIndex,
-							       info->mesgList[sendIndex]->msg, (long long) outTime.tv_sec,outTime.tv_nsec);
-							free(info->mesgList[sendIndex]);
-							info->mesgList[sendIndex] = NULL;
-							sendIndex = (sendIndex+1) % LIST_SIZE;
-						}
-					pthread_mutex_unlock( &listMutex );
-				}
-		}
-	printf("Thread Client ENDED\n");
-	pthread_exit((void*)0);
+        lagger(delay_avg, delay_variation);
+        if(strcmp(temp.msg, "") != 0)
+          sendto(sockfd, temp.msg, strlen(temp.msg), 0,(struct sockaddr*) &servaddr, sizeof(servaddr));
+
+        struct timespec outTime;
+        clock_gettime(CLOCK_MONOTONIC,&outTime);
+
+        printf("mesgList[%d]: %s\nOutbound timestamp: %lld.%09ld [s]\n", sendIndex,
+            temp.msg, (long long) outTime.tv_sec,outTime.tv_nsec);
+      }
+    }
+  }
+  printf("Thread Client ENDED\n");
+  pthread_exit((void*)0);
 }
 
 void* recieverThread(void* arg)
 {
-	SocketsInfo* info = (SocketsInfo*) arg;
-	printf("Thread Server Started on port: %d\n", info->local_port);
-	int sockfd = info->reciever, bytesRecieved;
-	struct sockaddr_in servaddr, cliaddr;
-	socklen_t len;
+  SocketsInfo* info = (SocketsInfo*) arg;
+  printf("Thread Server Started on port: %d\n", info->local_port);
+  int sockfd = info->reciever, bytesRecieved;
+  struct sockaddr_in servaddr, cliaddr;
+  socklen_t len;
 
-	bzero(&servaddr, sizeof(servaddr));
-	// Requisitos minimos de serveaddr para uso posterior
-	// INADDR_ANY: atender a cualquier cliente
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(info->local_port);
-	// Bindea al descriptor de archivo con el socketaddr del servidor
-	bind(sockfd,(struct sockaddr*) &servaddr, sizeof(servaddr));
+  bzero(&servaddr, sizeof(servaddr));
+  // Requisitos minimos de serveaddr para uso posterior
+  // INADDR_ANY: atender a cualquier cliente
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(info->local_port);
+  // Bindea al descriptor de archivo con el socketaddr del servidor
+  bind(sockfd,(struct sockaddr*) &servaddr, sizeof(servaddr));
 
-	for(;;)
-	{
-		len = sizeof(cliaddr);
-		bytesRecieved = recvfrom(sockfd,mesg,MSG_SIZE,0,(struct sockaddr *)&cliaddr,&len);
-		mesg[bytesRecieved] = '\0'; /* Sanitize message */
-		int error;
-		if ((error = pthread_mutex_lock( &listMutex )) != 0)
-			fprintf(stderr,"ERROR: %d\n", error);
-		else
-		{
-			if (NULL == info->mesgList[mesgIndex])
-			{
-				datagram *n = malloc(sizeof(datagram));
-				if (NULL == n)
-				{
-					fprintf(stderr,"ERROR: Couldn't allocate memory for datagram\n");
-					abort();
-				}
-				strcpy(n->msg,mesg);
-				clock_gettime(CLOCK_MONOTONIC,&n->timestamp);
-				info->mesgList[mesgIndex] = n;
+  for(;;)
+  {
+    len = sizeof(cliaddr);
+    bytesRecieved = recvfrom(sockfd,mesg,MSG_SIZE,0,(struct sockaddr *)&cliaddr,&len);
+    mesg[bytesRecieved] = '\0'; /* Sanitize message */
+    int error;
+    if ((error = pthread_mutex_lock( &listMutex )) != 0)
+      fprintf(stderr,"ERROR: %d\n", error);
+    else
+    {
+      if (NULL == info->mesgList[mesgIndex])
+      {
+        datagram *n = malloc(sizeof(datagram));
+        if (NULL == n)
+        {
+          fprintf(stderr,"ERROR: Couldn't allocate memory for datagram\n");
+          abort();
+        }
+        strcpy(n->msg,mesg);
+        clock_gettime(CLOCK_MONOTONIC,&n->timestamp);
+        info->mesgList[mesgIndex] = n;
 
-				printf("-------------------------------------------------------\n");
-				printf("Received the following:\n");
-				printf("Bytes recieved: %d\n",bytesRecieved);
-				printf("%s",info->mesgList[mesgIndex]->msg);
-				printf("Inbound timestamp: %lld.%09ld [s]\n", (long long) info->mesgList[mesgIndex]->timestamp.tv_sec, info->mesgList[mesgIndex]->timestamp.tv_nsec);
-				printf("-------------------------------------------------------\n");
+        printf("-------------------------------------------------------\n");
+        printf("Received the following:\n");
+        printf("Bytes recieved: %d\n",bytesRecieved);
+        printf("%s",info->mesgList[mesgIndex]->msg);
+        printf("Inbound timestamp: %lld.%09ld [s]\n", (long long) info->mesgList[mesgIndex]->timestamp.tv_sec, info->mesgList[mesgIndex]->timestamp.tv_nsec);
+        printf("-------------------------------------------------------\n");
 
-			}
-			else
-			{
-				printf("Buffer pos %d overwritten\n",mesgIndex);
-				sendIndex = (sendIndex+1) % LIST_SIZE;
-			}
-			mesgIndex=(mesgIndex+1)%LIST_SIZE;
+      }
+      else
+      {
+        printf("Buffer pos %d overwritten\n",mesgIndex);
+        sendIndex = (sendIndex+1) % LIST_SIZE;
+      }
+      mesgIndex=(mesgIndex+1)%LIST_SIZE;
 
-		}
-		pthread_mutex_unlock( &listMutex );
-	}
-	printf("Thread Server ENDED\n");
-	pthread_exit((void*)0);
+    }
+    pthread_mutex_unlock( &listMutex );
+  }
+  printf("Thread Server ENDED\n");
+  pthread_exit((void*)0);
 }
 
 void initList(datagram** list, int size)
 {
-	int i;
-	for(i=0; i<size; i++)
-	{
-		list[i] = NULL;
-	}
+  int i;
+  for(i=0; i<size; i++)
+  {
+    list[i] = NULL;
+  }
 }
 
 void lagger(double avg, double variation)
 {
-	double random;
-	double delay;
+  double random;
+  double delay;
 
-	if(randsafe(&random) != 0)
-		return;
-	printf("Random created = %f\n", random);
+  if(randsafe(&random) != 0)
+    return;
+  printf("Random created = %f\n", random);
 
-	/* Le doy un 2.5% mas de rango a la variacion total hacia arriba y hacia abajo */
-	double fixed_variation = variation*(1 + (double)loss_percent/100);
-	delay = 2*random*fixed_variation + avg - fixed_variation;
+  /* Le doy un 2.5% mas de rango a la variacion total hacia arriba y hacia abajo */
+  double fixed_variation = variation*(1 + (double)loss_percent/100);
+  delay = 2*random*fixed_variation + avg - fixed_variation;
 
-	if(delay > avg + variation || delay < avg - variation)
-	{
-		strcpy(mesg, "");
-		printf("**********************Packet Loss*********************\n");
-	}
-	printf("Delay: %f\n", delay);
+  if(delay > avg + variation || delay < avg - variation)
+  {
+    strcpy(mesg, "");
+    printf("**********************Packet Loss*********************\n");
+  }
+  printf("Delay: %f\n", delay);
 
-	if(delay > 1000)
-	{
-		/* Segun documentacion, no llamar a usleep con mas de 1M us*/
-		printf("Too much lag\n");
-		return;
-	}
+  if(delay > 1000)
+  {
+    /* Segun documentacion, no llamar a usleep con mas de 1M us*/
+    printf("Too much lag\n");
+    return;
+  }
 
-	usleep((int) delay *1000);
+  usleep((int) delay *1000);
 }
 
 int randsafe(double *ranp)
 {
-	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-	int error= 0;
+  static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+  int error= 0;
 
-	if ((error = pthread_mutex_lock(&lock)))
-		return error;
-	*ranp = (double)rand()/RAND_MAX;
-	return pthread_mutex_unlock(&lock);
+  if ((error = pthread_mutex_lock(&lock)))
+    return error;
+  *ranp = (double)rand()/RAND_MAX;
+  return pthread_mutex_unlock(&lock);
 }
